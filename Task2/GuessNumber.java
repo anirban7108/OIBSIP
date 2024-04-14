ade12c53121679657d5f707b5215ad798979bf47
anirban-mahapatra <anirbanmahapatra2003@gmail.com>
2024-04-14 10:40:22 +0530

    Code

--- Task2/GuessNumber.java
+++ Task2/GuessNumber.java
@@ -1,5 +1,26 @@
-package Task2;
+import java.util.*;
+
 public class GuessNumber {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int num = r.nextInt(1, 100), i = 1;
+        while (i <= 10) {
+            System.out.print("\nEnter the Guess:-");
+            int guess = sc.nextInt();
+            if (guess == num) {
+                System.out.println("\n\n\t\tWRIGHT GUESS\n\t\tNo of guess=" + i + "\n");
+                break;
+            } else if (guess < num) {
+                System.out.println("\nIt is lower then generated number");
+                i++;
+            } else if (guess > num) {
+                System.out.println("\nIt is higher then generated number");
+                i++;
+            } else {
+                System.out.println("\nWrong Input\n");
+            }
+            System.out.println("No of attempts left:" + (11 - i));
+        }
     }
 }
